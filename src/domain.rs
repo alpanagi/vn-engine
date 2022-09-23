955d9a57a28fc0b25ff612740e1dd40a9e51bb76
Alexandros Panagiotakis <dev@alpanagi.com>
2022-09-23 20:55:36 +0200

    #12 Character options, rudimentary syntax errors

--- src/domain.rs
+++ src/domain.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
 use bevy::prelude::*;
 
@@ -9,10 +9,14 @@ use crate::script_loader::Script;
 pub struct Screen {
     pub background: String,
     pub speaker: Option<String>,
+    pub speaker_color: Color,
     pub text: String,
 }
 
+pub type GameData = HashMap<String, HashMap<String, String>>;
+
 pub struct GameState {
+    pub data: GameData,
     pub unprocessed_scripts: Vec<Handle<Script>>,
     pub commands: VecDeque<Command>,
     pub history: Vec<(Command, Screen)>,
@@ -20,6 +24,7 @@ pub struct GameState {
 impl Default for GameState {
     fn default() -> Self {
         GameState {
+            data: HashMap::new(),
             unprocessed_scripts: vec![],
             commands: VecDeque::new(),
             history: vec![],
@@ -36,6 +41,7 @@ impl GameState {
         let screen = self.current_screen().unwrap_or(Screen {
             background: String::new(),
             speaker: Some(String::new()),
+            speaker_color: Color::WHITE,
             text: String::new(),
         });
 
@@ -50,11 +56,39 @@ impl GameState {
             Command::DisplayBackground { image_path } => Screen {
                 background: image_path.clone(),
                 speaker: screen.speaker.clone(),
+                speaker_color: Color::WHITE,
                 text: screen.text.clone(),
             },
+            Command::CharacterText { speaker, text } => {
+                let speaker_name: String = match self.data.get(speaker) {
+                    Some(data) => data.get("name").unwrap_or(&speaker).to_owned(),
+                    None => speaker.clone(),
+                };
+
+                let speaker_color: String = self
+                    .data
+                    .get(speaker)
+                    .and_then(|data| data.get("color"))
+                    .map(Clone::clone)
+                    .unwrap_or("".to_string());
+
+                let speaker_color: Color = match speaker_color.as_str() {
+                    "blue" => Color::BLUE,
+                    "red" => Color::RED,
+                    _ => Color::WHITE,
+                };
+
+                Screen {
+                    background: screen.background.clone(),
+                    speaker: Some(speaker_name),
+                    speaker_color: speaker_color,
+                    text: text.clone(),
+                }
+            }
             Command::Text { speaker, text } => Screen {
                 background: screen.background.clone(),
                 speaker: speaker.clone(),
+                speaker_color: Color::WHITE,
                 text: text.clone(),
             },
         }
